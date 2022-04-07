096071c28f95635ac164c17092ea147c2f40df70
Craig <Craig.samuelson01@outlook.com>
2022-04-07 09:48:40 +0200

    lots of changes after a lot of time

---
+++ Drumkitgame/Drum Kit Starting Files/index.js
@@ -0,0 +1,11 @@
+
+var y = document.getElementsByClassName("drum").length ; 
+for (var i = 0 ; i <= y ; i++ ) {
+document.querySelectorAll("button")[i].addEventListener("click",function(){
+    this.style.color="white";
+} );
+}
+    
+
+
+
